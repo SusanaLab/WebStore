7e7308239f0c9f019d70330fbda1ba3596c20f08
Susana Garcia <susanlevart@gmail.com>
2024-03-19 09:44:45 -0600

    Agregando codigo inicial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Footer/>
+      <Home/>
+   
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import "../styles/Navbar.css"
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="container">
+      <h2>Navbar</h2>
+      <ul>
+        <li>Inicio</li>
+        <li>Productos</li>
+        <li>Contactos</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar
