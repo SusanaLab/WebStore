38ef9dd9de060cc6e884b3bb998070df6b3f1a58
Susana Garcia <susanlevart@gmail.com>
2024-04-19 11:37:19 -0600

    Error page added

--- src/components/pages/Error/ErrorContainer.jsx
+++ src/components/pages/Error/ErrorContainer.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import { Button, Grid, Typography } from "@mui/material";
+import React from "react";
+import error from "../../../images/404.png";
+import { Link } from "react-router-dom";
 
 const ErrorContainer = () => {
   return (
-    <div>
-      Not found
-    </div>
-  )
-}
-
-export default ErrorContainer
+    <Grid item display="flex" justifyContent="center" alignItems="center">
+      <Grid item
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        item
+      >
+        <img
+          src={error}
+          alt="Error Logo"
+          style={{ width: "100%", maxWidth: 840, height: "auto" }}
+        />
+        <Typography
+          justifyContent="center"
+          alignItems="center"
+          variant="h5"
+          color="secondary"
+          gutterBottom
+        >
+          The page you’re looking for doesn't exist or has been moved
+        </Typography>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <Button
+            mt={6}
+            justifyContent="center"
+            alignItems="center"
+            variant="contained"
+            color="acento"
+          >
+            <Typography variant="h5">Go Home</Typography>
+          </Button>
+        </Link>
+      </Grid>
+    </Grid>
+  );
+};
+export default ErrorContainer;
